7303f6b5e9581086072a093335b0673e6f74db86
Alex Piechowski <alex@piechowski.io>
2017-11-22 16:01:34 -0600

    Bump year so example is in the future.

--- app/javascript/packs/countdown_timer.js
+++ app/javascript/packs/countdown_timer.js
@@ -4,6 +4,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const target = document.getElementById('countdown-timer')
 
   Elm.CountdownTimer.embed(target, {
-    deadline: "May 30 2017 14:05:00"
+    deadline: "May 30 2018 14:05:00"
   });
 })
